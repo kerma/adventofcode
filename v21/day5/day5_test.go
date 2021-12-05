be01a4689f3f6af815271b3bc1f32305a17dd829
Margus Kerma <margus@amrek.net>
2021-12-05 16:23:49 +0200

    day5.2: pen and paper

--- v21/day5/day5_test.go
+++ v21/day5/day5_test.go
@@ -41,52 +41,108 @@ func filterInt(ss []string) []int {
 	return r
 }
 
-type grid map[string]int
-
 func key(x, y int) string {
 	return fmt.Sprintf("%d|%d", x, y)
 }
 
-func run(r io.Reader) int {
-	var data [][]int
-	scanner := bufio.NewScanner(r)
-	for i := 0; scanner.Scan(); i++ {
-		m := pattern.FindStringSubmatch(scanner.Text())
-		data = append(data, filterInt(m))
-	}
+func getPoints(point []int) []string {
 
-	g := make(grid, 0)
-	for _, point := range data {
-		var x1, y1, x2, y2 = point[0], point[1], point[2], point[3]
-		if x1 == x2 { // vert
-			if y1 <= y2 {
-				for i := y1; i <= y2; i++ {
-					k := key(x1, i)
-					g[k] += 1
-				}
-			} else {
-				for i := y2; i <= y1; i++ {
-					k := key(x1, i)
-					g[k] += 1
-				}
+	var x1, y1, x2, y2 = point[0], point[1], point[2], point[3]
+	keys := make([]string, 0)
+	if x1 == x2 { // vert
+		if y1 <= y2 {
+			for i := y1; i <= y2; i++ {
+				keys = append(keys, key(x1, i))
 			}
-		} else if y1 == y2 { // horiz
-			if x1 <= x2 {
-				for i := x1; i <= x2; i++ {
-					k := key(i, y1)
-					g[k] += 1
-				}
-			} else {
-				for i := x2; i <= x1; i++ {
-					k := key(i, y1)
-					g[k] += 1
-				}
+		} else {
+			for i := y2; i <= y1; i++ {
+				keys = append(keys, key(x1, i))
 			}
 		}
-
+	} else if y1 == y2 { // horiz
+		if x1 <= x2 {
+			for i := x1; i <= x2; i++ {
+				keys = append(keys, key(i, y1))
+			}
+		} else {
+			for i := x2; i <= x1; i++ {
+				keys = append(keys, key(i, y1))
+			}
+		}
 	}
+	return keys
+}
+
+func getPointsV(point []int) []string {
+	var x1, y1, x2, y2 = point[0], point[1], point[2], point[3]
+	xs := make([]int, 0)
+	ys := make([]int, 0)
+	i := x1
+	for {
+		if i == x2 {
+			break
+		}
+		if i > x2 {
+			i--
+		} else {
+			i++
+		}
+		xs = append(xs, i)
+	}
+	i = y1
+	for {
+		if i == y2 {
+			break
+		}
+		if i > y2 {
+			i--
+		} else {
+			i++
+		}
+		ys = append(ys, i)
+	}
+	keys := make([]string, 0)
+	keys = append(keys, key(x1, y1))
+	for i, x := range xs {
+		keys = append(keys, key(x, ys[i]))
+	}
+	return keys
+}
+
+func isDiagonal(point []int) bool {
+	var x1, y1, x2, y2 = point[0], point[1], point[2], point[3]
+	if x1 == x2 || y1 == y2 {
+		return false
+	}
+	return true
+}
+
+func calculateKeys(point []int, diagonal bool) []string {
+	if isDiagonal(point) {
+		if diagonal {
+			return getPointsV(point)
+		} else {
+			return []string{}
+		}
+	}
+	return getPoints(point)
+}
+
+func run(r io.Reader, diagonal bool) int {
+	mapCounter := make(map[string]int, 0)
+	scanner := bufio.NewScanner(r)
+
+	for i := 0; scanner.Scan(); i++ {
+		m := pattern.FindStringSubmatch(scanner.Text())
+		point := filterInt(m)
+		keys := calculateKeys(point, diagonal)
+		for _, k := range keys {
+			mapCounter[k] += 1
+		}
+	}
+
 	var c int
-	for _, v := range g {
+	for _, v := range mapCounter {
 		if v >= 2 {
 			c++
 		}
@@ -98,7 +154,15 @@ func run(r io.Reader) int {
 func TestExample(t *testing.T) {
 	expect := 5
 	r := bytes.NewReader(example)
-	if got := run(r); expect != got {
+	if got := run(r, false); expect != got {
+		t.Fatalf("%d != %d\n", expect, got)
+	}
+}
+
+func TestExamplePartTwo(t *testing.T) {
+	expect := 12
+	r := bytes.NewReader(example)
+	if got := run(r, true); expect != got {
 		t.Fatalf("%d != %d\n", expect, got)
 	}
 }
@@ -112,7 +176,21 @@ func TestPartOne(t *testing.T) {
 	}
 	defer file.Close()
 
-	if got := run(file); expect != got {
+	if got := run(file, false); expect != got {
+		t.Fatalf("%d != %d\n", expect, got)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	expect := 17604
+
+	file, err := os.Open("input")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	if got := run(file, true); expect != got {
 		t.Fatalf("%d != %d\n", expect, got)
 	}
 }
