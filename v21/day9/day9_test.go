5b70c6b5fcfb2ccd8d518704d34df9fe1f9155b3
Margus Kerma <margus@amrek.net>
2021-12-10 20:34:27 +0200

    day9.1: refactor / simplify

--- v21/day9/day9_test.go
+++ v21/day9/day9_test.go
@@ -1,17 +1,16 @@
 package day9
 
 import (
+	"bufio"
 	"bytes"
+	"fmt"
 	"io"
 	"os"
 	"testing"
-
-	"github.com/kerma/adventofcode/v21/util"
 )
 
 var (
-	example = []byte(`
-2199943210
+	example = []byte(`2199943210
 3987894921
 9856789892
 8767896789
@@ -19,61 +18,73 @@ var (
 `)
 )
 
-func findLows(ns [][]int) []int {
+type position struct {
+	x, y, v int
+}
 
-	adjacent := func(x, y int, data [][]int) []int {
-		h := len(data)
-		w := len(data[0])
-		a := make([]int, 0)
+func (p position) String() string { return fmt.Sprintf("[v:%d x:%d y:%d]", p.v, p.x, p.y) }
 
-		prev := x - 1
-		if prev >= 0 {
-			a = append(a, data[y][prev])
-		}
+func adjacent(x, y int, data [][]int) []position {
+	h := len(data)
+	w := len(data[0])
+	a := make(positions, 0)
 
-		next := x + 1
-		if next < w {
-			a = append(a, data[y][next])
-		}
-
-		up := y - 1
-		if up >= 0 {
-			a = append(a, data[up][x])
-		}
-
-		down := y + 1
-		if down < h {
-			a = append(a, data[down][x])
-		}
-
-		return a
+	prev := x - 1
+	if prev >= 0 {
+		a = append(a, position{prev, y, data[y][prev]})
 	}
 
-	isLow := func(n int, ns []int) bool {
-		for _, v := range ns {
-			if n >= v {
+	next := x + 1
+	if next < w {
+		a = append(a, position{next, y, data[y][next]})
+	}
+
+	up := y - 1
+	if up >= 0 {
+		a = append(a, position{x, up, data[up][x]})
+	}
+
+	down := y + 1
+	if down < h {
+		a = append(a, position{x, down, data[down][x]})
+	}
+
+	return a
+}
+
+func run(r io.Reader) int {
+
+	var fields [][]int
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		var line []int
+		for _, v := range s.Text() { // C way to parse str to int
+			line = append(line, int(byte(v)-byte('0')))
+		}
+		fields = append(fields, line)
+	}
+
+	isLow := func(n int, ps []position) bool {
+		for _, p := range ps {
+			if n >= p.v {
 				return false
 			}
 		}
 		return true
 	}
 
-	lows := make([]int, 0)
-	for y, line := range ns {
+	var sum, count int
+	for y, line := range fields {
 		for x, n := range line {
-			a := adjacent(x, y, ns)
+			a := adjacent(x, y, fields)
 			if isLow(n, a) {
-				lows = append(lows, n)
+				sum += n
+				count++
 			}
 		}
 	}
-	return lows
-}
 
-func run(r io.Reader) int {
-	ns := util.ReadCharsToInts(r)
-	lows := findLows(ns)
-	return util.Sum(lows) + len(lows)
+	return sum + count
 }
 
 func TestExample(t *testing.T) {
@@ -97,3 +108,11 @@ func TestPartOne(t *testing.T) {
 		t.Fatalf("%d != %d\n", expect, got)
 	}
 }
+
+// func TestExamplePartTwo(t *testing.T) {
+// 	expect := 1134
+// 	r := bytes.NewReader(example)
+// 	if got := run2(r); expect != got {
+// 		t.Fatalf("%d != %d\n", expect, got)
+// 	}
+// }
