9d5960e6641e22e78c6e7e94da6fdf0d5d59e913
Margus Kerma <margus@amrek.net>
2021-12-12 22:56:41 +0200

    day11.2: reusable part 1

--- v21/day11/day11_test.go
+++ v21/day11/day11_test.go
@@ -20,16 +20,6 @@ var (
 4846848554
 5283751526`)
 
-	example3 = []byte(`111
-199
-111`)
-
-	example2 = []byte(`11111
-19991
-19191
-19991
-11111`)
-
 	input = []byte(`2264552475
 7681287325
 3878781441
@@ -47,8 +37,8 @@ func adjacent(x, y int, data [][]int) [][]int {
 
 	var coords [][]int
 	for line := -1; line <= 1; line++ {
-		for row := -1; row <= 1; row++ {
-			x1, y1 := x+line, y+row
+		for col := -1; col <= 1; col++ {
+			x1, y1 := x+line, y+col
 			if (x == x1 && y == y1) || x1 < 0 || x1 >= w || y1 < 0 || y1 >= h {
 				continue
 			}
@@ -60,16 +50,16 @@ func adjacent(x, y int, data [][]int) [][]int {
 }
 
 func increaseAdjecent(x, y int, ns [][]int) int {
-	var inc int
+	var count int
 	for _, a := range adjacent(x, y, ns) {
 		x, y := a[0], a[1]
 		if ns[y][x] == 10 || ns[y][x] == 0 {
 			continue
 		}
 		ns[y][x]++
-		inc++
+		count++
 	}
-	return inc
+	return count
 }
 
 func walkFlashed(levels [][]int, counter *int) {
@@ -78,7 +68,7 @@ func walkFlashed(levels [][]int, counter *int) {
 			if levels[y][x] == 10 {
 				levels[y][x] = 0
 				*counter++
-				if inc := increaseAdjecent(x, y, levels); inc > 0 {
+				if changed := increaseAdjecent(x, y, levels); changed > 0 {
 					walkFlashed(levels, counter)
 				}
 			}
@@ -86,8 +76,15 @@ func walkFlashed(levels [][]int, counter *int) {
 	}
 }
 
+func increase(ns [][]int) {
+	for y, line := range ns {
+		for x, _ := range line {
+			ns[y][x]++
+		}
+	}
+}
+
 func run(r io.Reader) int {
-	var totalSteps = 100
 	var levels [][]int
 
 	s := bufio.NewScanner(r)
@@ -99,20 +96,47 @@ func run(r io.Reader) int {
 		levels = append(levels, line)
 	}
 
-	increase := func(ns [][]int) {
-		for y, line := range ns {
-			for x, _ := range line {
-				ns[y][x]++
-			}
-		}
-	}
-
 	var totalFlashes int
-	for step := 1; step <= totalSteps; step++ {
+	for step := 1; step <= 100; step++ {
 		increase(levels)
 		walkFlashed(levels, &totalFlashes)
 	}
 	return totalFlashes
+}
+
+func run2(r io.Reader) int {
+	var levels [][]int
+
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		var line []int
+		for _, v := range s.Text() { // C way to parse str to int
+			line = append(line, int(byte(v)-byte('0')))
+		}
+		levels = append(levels, line)
+	}
+
+	allFlashed := func() bool {
+		for y, line := range levels {
+			for x, _ := range line {
+				if levels[y][x] != 0 {
+					return false
+				}
+			}
+		}
+		return true
+	}
+
+	var total, na int
+	for {
+		total++
+		increase(levels)
+		walkFlashed(levels, &na)
+		if allFlashed() {
+			break
+		}
+	}
+	return total
 }
 
 func print(f [][]int) {
@@ -143,20 +167,20 @@ func TestPartOne(t *testing.T) {
 	}
 }
 
-// func TestExamplePartTwo(t *testing.T) {
-// 	expect := 195
-//
-// 	r := bytes.NewReader(example)
-// 	if got := run2(r); expect != got {
-// 		t.Fatalf("%d != %d\n", expect, got)
-// 	}
-// }
+func TestExamplePartTwo(t *testing.T) {
+	expect := 195
 
-// func TestPartTwo(t *testing.T) {
-// 	expect := -1
-//
-// 	r := bytes.NewReader(example)
-// 	if got := run2(r); expect != got {
-// 		t.Fatalf("%d != %d\n", expect, got)
-// 	}
-// }
+	r := bytes.NewReader(example)
+	if got := run2(r); expect != got {
+		t.Fatalf("%d != %d\n", expect, got)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	expect := 303
+
+	r := bytes.NewReader(input)
+	if got := run2(r); expect != got {
+		t.Fatalf("%d != %d\n", expect, got)
+	}
+}
