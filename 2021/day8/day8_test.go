c7ea01148089f5d3b43066016a0a97bc678ea5ae
Margus Kerma <margus@amrek.net>
2021-12-13 21:31:42 +0200

    day8: intersect

--- 2021/day8/day8_test.go
+++ 2021/day8/day8_test.go
@@ -3,11 +3,13 @@ package day7
 import (
 	"bufio"
 	"bytes"
-	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
+
+	"github.com/kerma/adventofcode/2021/util"
 )
 
 var (
@@ -24,11 +26,9 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 `)
 )
 
-type data [][]string
+func read(r io.Reader) ([][]string, [][]string) {
 
-func read(r io.Reader) (data, data) {
-
-	var patterns, outputs data
+	var patterns, outputs [][]string
 
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
@@ -52,12 +52,12 @@ func read(r io.Reader) (data, data) {
 	return patterns, outputs
 }
 
-func filter(d data) data {
-	var f data
+func filter(d [][]string) [][]string {
+	var f [][]string
 	for _, line := range d {
 		var tmp []string
 		for _, p := range line {
-			if len(p) != 1 && len(p) != 5 && len(p) != 6 {
+			if len(p) != 5 && len(p) != 6 {
 				tmp = append(tmp, p)
 			}
 		}
@@ -69,13 +69,83 @@ func filter(d data) data {
 func run(r io.Reader) int {
 	_, patterns := read(r)
 	patterns = filter(patterns)
-	fmt.Printf("%v\n", patterns)
 
 	var c int
 	for _, line := range patterns {
 		c += len(line)
 	}
 	return c
+}
+
+type Set map[rune]bool
+
+func (s Set) Intersect(s2 Set) Set {
+	inter := map[rune]bool{}
+	for a, _ := range s {
+		if s2[a] {
+			inter[a] = true
+		}
+	}
+	return inter
+}
+
+func NewSet(str string) Set {
+	set := make(Set)
+	for _, r := range str {
+		set[r] = true
+	}
+	return set
+}
+
+func run2(r io.Reader) int {
+
+	lines, outputs := read(r)
+
+	values := make([]int, len(lines))
+	for i, signals := range lines {
+		knownSets := make(map[int]Set)
+		for _, sig := range signals {
+			if len(sig) == 2 || len(sig) == 4 {
+				knownSets[len(sig)] = NewSet(sig)
+			}
+		}
+
+		decoded := ""
+		for _, output := range outputs[i] {
+			switch len(output) {
+			case 2:
+				decoded += "1"
+			case 3:
+				decoded += "7"
+			case 4:
+				decoded += "4"
+			case 7:
+				decoded += "8"
+			case 5:
+				s := NewSet(output)
+				if len(s.Intersect(knownSets[2])) == 2 {
+					decoded += "3"
+				} else if len(s.Intersect(knownSets[4])) == 2 {
+					decoded += "2"
+				} else {
+					decoded += "5"
+				}
+			default: // len == 6
+				s := NewSet(output)
+				if len(s.Intersect(knownSets[2])) == 1 {
+					decoded += "6"
+				} else if len(s.Intersect(knownSets[4])) == 4 {
+					decoded += "9"
+				} else {
+					decoded += "0"
+				}
+			}
+		}
+		v, _ := strconv.Atoi(decoded)
+		values[i] = v
+	}
+
+	return util.Sum(values)
 }
 
 func TestExample(t *testing.T) {
@@ -96,6 +166,29 @@ func TestPartOne(t *testing.T) {
 	defer file.Close()
 
 	if got := run(file); expect != got {
+		t.Fatalf("%d != %d\n", expect, got)
+	}
+}
+
+func TestExamplePartTwo(t *testing.T) {
+	expect := 61229
+
+	r := bytes.NewReader(example)
+	if got := run2(r); expect != got {
+		t.Fatalf("%d != %d\n", expect, got)
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	expect := 990964
+
+	file, err := os.Open("input")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	if got := run2(file); expect != got {
 		t.Fatalf("%d != %d\n", expect, got)
 	}
 }
